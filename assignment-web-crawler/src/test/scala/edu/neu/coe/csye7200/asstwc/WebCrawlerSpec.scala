71e7fc4213221aa47b53237fa1a6bcf066839c1b
Yiqing Huang <huang.yiqin@northeastern.edu>
2022-04-03 15:33:51 -0400

    Complete A7

--- assignment-web-crawler/src/test/scala/edu/neu/coe/csye7200/asstwc/WebCrawlerSpec.scala
+++ assignment-web-crawler/src/test/scala/edu/neu/coe/csye7200/asstwc/WebCrawlerSpec.scala
@@ -17,6 +17,7 @@ import scala.util._
 class WebCrawlerSpec extends AnyFlatSpec with should.Matchers with Futures with ScalaFutures with TryValues with Inside {
 
   val goodURL = "http://www1.coe.neu.edu/~rhillyard/indexSafe.html"
+  val correctURL = "https://coe.northeastern.edu/people/xu-mofei/"
   val badURL = "http://www1.coe.neu.edu/junk"
 
   "getURLContent" should s"succeed for $goodURL" taggedAs Slow in {
@@ -72,12 +73,12 @@ class WebCrawlerSpec extends AnyFlatSpec with should.Matchers with Futures with 
     }
   }
 
-  "crawler(Seq[URL])" should s"succeed for $goodURL, depth 2" taggedAs Slow in {
-    val args = List(s"$goodURL")
+  "crawler(Seq[URL])" should s"succeed for $correctURL, depth 2" taggedAs Slow in {
+    val args = List(s"$correctURL")
     val uys = for (arg <- args) yield Try(new URL(arg))
     val usft = for {us <- MonadOps.sequenceForgiving(uys)} yield WebCrawler.crawler(2, us)
     val usf = MonadOps.flatten(usft)
-    whenReady(usf, timeout(Span(60, Seconds))) { s => Assertions.assert(s.length == 35) }
+    whenReady(usf, timeout(Span(60, Seconds))) { s => Assertions.assert(s.length == 60) }
   }
 
   //  "crawler(Seq[URL])" should "succeed for test.html, depth 2" in {
